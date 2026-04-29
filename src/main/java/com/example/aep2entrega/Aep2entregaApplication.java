b7d7fe3b70ec72486f3f2e10abd703e4a607c345
gmiashiro <g.miashiro.freitas@gmail.com>
2026-04-29 13:09:41 -0300

    adicionando projeto

---
+++ src/main/java/com/example/aep2entrega/Aep2entregaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aep2entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aep2entregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aep2entregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aep2entrega/Aep2entregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aep2entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aep2entregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
