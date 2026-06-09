4078fb787ed288bb5d425161bab48febaaa34fb7
gmiashiro <g.miashiro.freitas@gmail.com>
2026-06-09 19:42:19 -0300

    adicionando conexão do front com o back

---
+++ src/main/java/com/example/aep2entrega/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.aep2entrega.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+
+                .formLogin(form -> form.disable())
+
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
+}
